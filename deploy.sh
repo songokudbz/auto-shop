9eafdb37e7f0509c428323626fcf1ef293cb1666
Adrian <madaras_adrian@yahoo.com>
2018-09-30 17:33:52 +0200

    Added mock list of cars

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+ng build --prod
 set +
 aws s3 sync ./dist/terraform/ s3://carshop.timafe.net/ --region=eu-central-1 --delete --profile=car-shop-manager
 set -
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from "./app.routing";
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { ListCarsComponent } from './list-cars/list-cars.component';
+import { CarService } from "./service/car.service";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
